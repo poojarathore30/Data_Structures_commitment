1b907143eb753dc77bb86a0386edba2f40c7c447
Pooja Rathore <poojarathore.nks@gmail.com>
2020-11-11 01:55:48 +0530

    Create Remove K digits to make smallest number.cpp

---
+++ Strings/Remove K digits to make smallest number.cpp
@@ -0,0 +1,33 @@
+// num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+// Input: num = "1432219", k = 3
+// Output: "1219"
+//solution : Using stack to make the graph as flattened as possible 
+// approach : pop the top element out of stack if curr ele is smaller and k>0 to fir only the smallest possible into the stack
+
+string removeKdigits(string num, int k) {
+ stack<char,int> s;
+ for(auto i:nums)
+ {
+ while(s.empty() && k>0 && s.top()>i) 
+ {
+ s.pop();
+ k--;
+ }
+ s.push(c);
+ }
+ while(k>0)
+ {
+ s.pop();
+ k--;
+ }
+ string str="";
+ while(!s.empty())
+ {
+str=s.top()+str;
+s.pop();
+ }
+int start=0;
+while(str[start]=='0') start++;
+string res=str.substr(start,str.length());
+ return res;
+ }
